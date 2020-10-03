c0bae248a6175e0bbfab7eb604759c6a2e355909
Zach Jessen <z.jessen@gmail.com>
2020-10-03 12:38:28 -0500

    moved gui into react app

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# bahavior_rig
+# behavior_rig
 Schwartz Lab Behavior Rig Project
+
+## Web gui
+To run test scripts:
+1. Install node/npm
+2. `cd` into `client` and run `npm install` followed by `npm run build`
+3. `cd` into `server` and run `python3 test_webserving.py`
+4. Open a browser directed at `localhost:3001` and you should see 4 video streams playing white noise
+    - Pressing the button will send a `POST` request to the Flask server which can be used to execute callbacks such as to start recording
--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+
+  render() {
+    return (
+      <div className="App">
+        <p>{window.token}</p>
+        <img src='/camera0'/>
+        <img src='/camera1'/>
+        <img src='/camera2'/>
+        <img src='/camera3'/>
+        <button onClick={this.onClick}>Click me</button>
+      </div>
+    );
+  }
+
+  onClick = () => {
+    fetch('/api/', {
+      method: 'POST',
+      body: '',
+    });
+  }
+  
 }
 
 export default App;
