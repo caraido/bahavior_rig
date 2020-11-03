d64837aa2f9b5b775892464267d95058e366e54f
Zach Jessen <z.jessen@gmail.com>
2020-11-03 09:11:30 -0600

    adding comments

--- main.py
+++ main.py
@@ -7,12 +7,25 @@ audio_settings = {
     'fs': 3e5,  # sample rate
     'fMin': 20000,
     'fMax': 115000,
-    'nFreq': 5e3,  # number of frequency values to send
+    'nFreq': 5e3,  # number of frequencies to plot
     'fScale': 'log',  # frequency spacing, linear or log
     'window': .0032,  # length of window in seconds
     'overlap': .875,  # fractional overlap
     'correction': True, #whether to correct for 1/f noise
-    'readRate': 1, #how frequently to read off the Daq
+    'readRate': 1, #how frequently to read data from the Daq's off-board data buffer
+    
+    #notes on parameters:
+    
+    # window, overlap, fMin, fMax are taken from deepsqueak
+    # fs needs to be at least twice the highest frequency of interest, ideally higher
+    # window*fs should be a power of 2 (or at least even) for optimal computation of fft
+    # the higher the readRate, the better the performance, but the latency of plotting increases
+
+    # nFreq determines the number of frequencies that are plotted (by cubic interpolation), not calculated
+    # the browser also performs some interpolation, so nFreq should be as low as possible to see features of interest
+
+
+
 }
 app = Flask(__name__)
 socketio = SocketIO(app, async_mode='threading', cors_allowed_origins=[])
